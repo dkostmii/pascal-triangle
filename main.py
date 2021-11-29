3b3c0be4a658f5c13c464acf178e8defc8808c55
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2021-11-29 13:50:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,72 @@
 import sys
-import os
 from math import factorial
 
+
 def main():
+    # default triangle height
     n = 20
 
-    if (len(sys.argv) > 1):
+    # parse argument
+    if len(sys.argv) > 1:
         try:
+            if int(sys.argv[1]) < 0:
+                raise Exception(f"Expected n to be non-negative. Got: {sys.argv[1]}")
+
             n = int(sys.argv[1])
-        except Exception:
+        except ValueError:
             print(f"Expected argument to be integer. Got: {sys.argv[1]}")
-            os._exit(1)
+            sys.exit(1)
+        except Exception as e:
+            if (hasattr(e, 'message')):
+                print(f"Error occurred: {e.message}")
+            else:
+                print(f"Error occurred: {e}")
+            sys.exit(1)
 
     results = []
 
+    # write values to results list
     for i in range(n + 1):
         results.append(list(map(int, binomial(i))))
 
-    max_digits = larges_number_lentgth(results[-1])
+    # get largest number digits count
+    # to center all numbers
+    max_digits = largest_number_length(results[-1])
 
     lines = []
     for i in range(n + 1):
         lines.append(list_to_line(list(map(int, binomial(i))), max_digits))
 
+    # center lines with respect to last
     for line in lines:
         print(line.center(len(lines[-1])))
 
 
-def larges_number_lentgth(numbers):
-    if (not isinstance(numbers, list)):
+def largest_number_length(numbers):
+    if not isinstance(numbers, list):
         raise Exception("Expected numbers to be list")
 
+    # map number to it's digits count
     return max(list(map((lambda n: len(str(n))), numbers)))
 
 
 def list_to_line(lst, places=None):
-    if (not isinstance(lst, list)):
+    if not isinstance(lst, list):
         raise Exception("Expected lst to be list")
-    if (places is None):
+    if places is None:
         return ' '.join([str(value) for value in lst])
 
+    # center numbers in line with
+    # for example 1 and max number 12345 result is: __1__
+    # where _ is whitespace
     return ' '.join([str(value).center(places) for value in lst])
 
 
 def binomial(n):
     coeffs = []
     for i in range(n + 1):
+        # calculate the binomial coefficient
+        # n choose k = n! / k! * (n - k)!
         coeffs.append(factorial(n) / (factorial(i) * factorial(n - i)))
 
     return coeffs
